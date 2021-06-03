262b48a03ea79524a1cf7a19be3331ec207552e8
Maxim <by_ke@mail.ru>
2021-06-03 17:06:53 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='orm_converter',
+    version='0.0.1',
+    packages=['orm_converter'],
+    url='',
+    license='',
+    author='Maxim',
+    author_email='maximzayats1@gmail.com',
+    description=''
+)
