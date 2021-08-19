747b04006450eebb7511614d3f52483274adfd10
MaximZayats <by_ke@mail.ru>
2021-08-19 23:55:55 +0300

    update: updated package url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='orm_converter',
     version='0.1.2a',
     packages=['orm_converter'],
-    url='',
+    url='https://github.com/MaximZayats/orm-converter',
     license='',
     author='Maxim',
     author_email='maximzayats1@gmail.com',
